a0b0df055d972413a8f309817080d9634dba0f59
flagrantii <tanuson679@gmail.com>
2023-06-08 14:53:02 +0700

    add genremovie

---
+++ src/component/GenreMovieList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GenresList from '../Co'
+
+function GenreMovieList() {
+  return (
+    <div>
+        {GenresList.genere.map()}
+    </div>
+  )
+}
+
+export default GenreMovieList
