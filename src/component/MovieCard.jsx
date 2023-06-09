8ee82f9ba49b92169cfab3c5327fc43e352eba5c
flagrantii <tanuson679@gmail.com>
2023-06-09 15:23:17 +0700

    add poster slide

---
+++ src/component/MovieCard.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+const IMAGE_BASE_URL="http://image.tmdb.org/t/p/original";
+
+function MovieCard({movie}) {
+  return (
+    <>
+        <img src={IMAGE_BASE_URL+movie.poster_path} 
+        className='w-[110px] md:w-[220px] rounded-lg
+        hover:border-[3px] border-gray-400 cursor-pointer
+        hover:scale-110 transition-all duration-150 ease-in'/>
+    </>
+  )
+}
+
+export default MovieCard
